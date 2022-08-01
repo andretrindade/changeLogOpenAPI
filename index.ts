4b650fc473b28b83dada8cfba47d639312cbab42
André Trindade <andre.trindade@sensedia.com>
2022-08-01 11:01:43 -0300

    feat - Adding rote endpoint

--- index.ts
+++ index.ts
@@ -1,24 +1,27 @@
 // TODO - em construção. 
 // Favor utilizar mecanismos de testes para validar aplicação. 
-// npm test
+// npx ts-node index.ts
 
-import ChangeLogGeneratorService from "./src/services/ChangeLogGeneratorService";
-import apiFase2 from "./src/documents/ApiFase2.json"
-let changeLogGeneratorService = new ChangeLogGeneratorService();
+import express from 'express';
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
+import { ChangeLogRoute } from './src/routes/ChangeLogRoute';
+import ChangeLogGeneratorService from './src/services/ChangeLogGeneratorService';
+const app = express();
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-apiFase2.url
+let changeLogRoute = new ChangeLogRoute(new ChangeLogGeneratorService());
 
-apiFase2.apis.forEach(api => {
-    api.versoes.forEach(async x => {
+// public 
+app.use('/change-log',changeLogRoute.montaRotas() );
+app.listen(3000);
+console.log('App started');
 
-        let urlOld = `${apiFase2.url}/${api.api}/${x.old}`;
-        let urlCurrent = `${apiFase2.url}/${api.api}/${x.current}`;
-        console.log(api.api)
-        console.log(urlOld)
-        console.log(urlCurrent)
-        console.log("------")
-
-        await changeLogGeneratorService.GenerateChangeLogWithUrlYaml(urlOld, urlCurrent);
-    });
-})
